aaef6048ed425630ac924aa2d547fc751fdc73c5
PauliusKu <paulius.kuprevicius@gmail.com>
2018-06-07 17:56:16 +0300

    Initial commit

---
+++ newVecctor/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "newVector_Header.h"
+
+int main()
+{
+	system("pause");
+	return 0;
+}
---
+++ newVecctor/newVector_Header.h
@@ -0,0 +1,28 @@
+#ifndef NEWVECTOR_HEADER_H
+#define NEWVECTOR_HEADER_H
+
+template<
+	class T,
+	class Alloc = std::allocator<T>
+> class vector {
+public:
+
+	typedef T 																value_type;//+
+	typedef Alloc 															allocator_type;//+
+	typedef size_t															size_type; //+
+	typedef std::ptrdiff_t 													difference_type; //+
+	typedef value_type& 													reference; //+
+	typedef const value_type& 												const_reference; //+
+	typedef typename std::allocator_traits<Alloc>::pointer 					pointer; //+
+	typedef typename std::allocator_traits<Alloc>::const_pointer 			const_pointer; //+
+	typedef T*																iterator; //+
+	typedef const T*														const_iterator; //+
+	typedef std::reverse_iterator<iterator> 								reverse_iterator;//+
+	typedef std::reverse_iterator<const_iterator> 							const_reverse_iterator;//+
+
+private:
+
+
+};
+
+#endif
